9e0ed503394434a0fcf3c214eaaed883d0c9be98
Xavi Martinez <xavi12p@hotmail.com>
2020-12-20 13:34:25 +0100

    pending to add comments

--- scripts/accion.js
+++ scripts/accion.js
@@ -1,49 +1,70 @@
-/**
- * Función que realiza la petición fetch y coloca el mapa en el documento
- * en el elemento indicado
- * @param {string} apikey
- * @param {Element} elemento Contenedor de la imagen y el formulario
- * */
-async function procesarFetch(apikey, elemento) {
-  let form = elemento.querySelector('form');
-  var headers = new Headers({
-    'cache-control': 'no-cache',
-  });
-  var conf = {
-    method: 'GET',
-    mode: 'cors',
-    headers: headers,
-  };
-  try {
-    const resp1 = await fetch(
-      'https://opendata.aemet.es/opendata/api/mapasygraficos/analisis?api_key=' +
-        apikey,
-      conf
-    );
-    const data1 = await resp1.json();
-    const resp2 = await fetch(data1.datos, conf);
-    const mapa = await resp2.blob();
-    const img = document.createElement('img');
-    img.setAttribute('src', URL.createObjectURL(mapa));
-    //Añadimos la imagen y quitamos el formulario
-    elemento.removeChild(form);
-    elemento.appendChild(img);
-  } catch (error) {
-    let p = document.createElement('p');
-    p.innerHTML = '<strong>Error al cargar el mapa:</strong> ' + error;
-    elemento.appendChild(p);
-  }
-}
+const BASE_URL = 'https://reqres.in/api/users';
+const POSTMAN_URL = 'https://httpbin.org/post';
 
 //Código principal dentro del evento load
 // para asegurar la carga de los componentes
 window.addEventListener('load', (ev) => {
-  let main = document.querySelector('main');
-  let apiKey = document.querySelector('textarea');
+  let numsecs = document.getElementById('numsecs');
+  let user = document.getElementById('user');
   let boton = document.querySelector('button');
 
   boton.addEventListener('click', (ev) => {
     ev.preventDefault();
-    procesarFetch(apiKey.value, main);
+    procesarFetch(numsecs.value, user.value - 1);
   });
 });
+
+function listUsuariosConf() {
+  let headers = new Headers({
+    'cache-control': 'no-cache',
+  });
+
+  let conf = {
+    method: 'GET',
+    mode: 'cors',
+    headers: headers,
+  };
+  return conf;
+}
+
+function createUsuariosConf(user) {
+  let conf = {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(user),
+  };
+  return conf;
+}
+
+function procesarFetch(numSecs, user) {
+  fetch(BASE_URL + '?delay=' + numSecs, listUsuariosConf())
+    .then((resultado) => resultado.json())
+    .then((resultadoJson) => mostrarUsuario(resultadoJson.data[user]))
+    .then((person) => fetch(POSTMAN_URL, createUsuariosConf(person)))
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      document.getElementById('name').innerHTML = json.json.first_name;
+    });
+}
+
+async function procesarFetchAwait(numSecs, user) {
+  // devuelve una promesa, y en resultado tenemos el response.
+  const resultado = await fetch(
+    BASE_URL + '?delay=' + numSecs,
+    listUsuariosConf()
+  );
+  // response.json() returns a promise resolved to a JSON object
+  const json = await resultado.json();
+  const person = mostrarUsuario(json.data[user]);
+
+  // devuelve una promesa, y en resultado2 tenemos el response.
+  const resultado2 = await fetch(POSTMAN_URL, createUsuariosConf(person));
+  const json2 = await resultado2.json();
+  document.getElementById('name').innerHTML = json2.json.first_name;
+}
+
+function mostrarUsuario(json) {
+  document.getElementById('id').innerHTML = json.id;
+  document.getElementById('email').innerHTML = json.email;
+  return json;
+}
